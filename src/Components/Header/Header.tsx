16f97b8c73a3beef757c1cf6b7f7394633a500e6
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-04-06 01:13:20 +0100

    add icons

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,9 +1,13 @@
 import * as Styled from './styles';
+import SettingsIcon from '@mui/icons-material/Settings';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Header = () => {
   return (
-    <Styled.Header>
+    <Styled.Header justify="space-between">
+      <MenuIcon sx={{ fontSize: 30, color: 'white', cursor: 'pointer' }} />
       <Styled.Title>CineGuessr</Styled.Title>
+      <SettingsIcon sx={{ fontSize: 30, color: 'white', cursor: 'pointer' }} />
     </Styled.Header>
   );
 };
