81c45d27f8d16ebbbc0e0fa704dae43c7b10bbe8
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-04-06 23:42:20 +0100

    add flex-direction

--- src/Components/styled-utils/mixins.ts
+++ src/Components/styled-utils/mixins.ts
@@ -4,10 +4,12 @@ interface FlexProps {
   readonly align?: string;
   readonly justify?: string;
   readonly gap?: number;
+  readonly direction?: string;
 }
 
 export const flex = css<FlexProps>`
   display: flex;
+  flex-direction: ${({ direction }) => direction || 'row'};
   align-items: ${({ align }) => align || 'center'};
   justify-content: ${({ justify }) => justify || 'center'};
   gap: ${({ gap }) => gap || 2}rem;
