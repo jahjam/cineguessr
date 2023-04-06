1ed2ff659f72380109ddd02f900bf0f7fa7c774d
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-04-06 23:42:11 +0100

    add icon styles

--- src/Components/Header/styles.ts
+++ src/Components/Header/styles.ts
@@ -1,10 +1,14 @@
 import styled from 'styled-components';
+import { ReactComponent as InfoIconSVG } from '../../assets/info.svg';
+import { ReactComponent as SettingsIconSVG } from '../../assets/settings.svg';
 import { flex } from '../styled-utils/mixins';
 
 export const Header = styled.header`
   background-color: var(--black-theme-color);
-  height: 8rem;
+  height: 10rem;
   padding: 2rem;
+
+  border-bottom: solid 0.2rem var(--white-theme-color);
 
   ${flex}
 `;
@@ -12,4 +16,29 @@ export const Header = styled.header`
 export const Title = styled.h1`
   font-family: var(--title-theme-font-family);
   color: var(--white-theme-color);
+`;
+
+export const InfoIcon = styled(InfoIconSVG)`
+  height: 3.2rem;
+  color: var(--white-theme-color);
+
+  cursor: pointer;
+`;
+
+export const SettingsIcon = styled(SettingsIconSVG)`
+  height: 3.2rem;
+  color: var(--white-theme-color);
+
+  cursor: pointer;
+`;
+
+export const HeaderContainer = styled.div`
+  ${flex}
+`;
+
+export const SubTitle = styled.span`
+  font-size: 1.4rem;
+  color: var(--white-theme-color);
+
+  align-self: flex-end;
 `;
