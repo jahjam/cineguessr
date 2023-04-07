563df31727e59a10e15f53ea2a97929af6ce0397
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-04-07 12:04:39 +0100

    added styles to details comp

---
+++ src/Components/Details/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { flex } from '../styled-utils/mixins';
+
+export const Container = styled.section`
+  height: 10rem;
+  width: 100%;
+
+  background-color: white;
+
+  ${flex}
+`;
