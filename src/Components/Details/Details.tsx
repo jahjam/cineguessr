6ad0a3a1aee49364075883813c514d6216027c2d
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-04-08 00:39:06 +0100

    add details jsx

--- src/Components/Details/Details.tsx
+++ src/Components/Details/Details.tsx
@@ -2,15 +2,18 @@ import * as Styled from './styles';
 
 const Details = () => {
   return (
-    <Styled.Container>
-      <h2>HINT</h2>
-      <div>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-      <div></div>
+    <Styled.Container justify="space-between">
+      <Styled.Hint>HINT</Styled.Hint>
+      <Styled.Data direction="column" gap={0.1}>
+        <span>Winning Streak: 2</span>
+        <span>Average Tries: 3.4</span>
+        <span>Least Tries: 3</span>
+        <span>Average Guess Time: 2m 53s</span>
+      </Styled.Data>
+      <Styled.Score>
+        <Styled.ClapperboardIcon />
+        <span>3</span>
+      </Styled.Score>
     </Styled.Container>
   );
 };
