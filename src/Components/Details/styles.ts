04e146de215ab96d63c4a45776acefd9c5abb72c
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-04-08 00:39:14 +0100

    add details styles

--- src/Components/Details/styles.ts
+++ src/Components/Details/styles.ts
@@ -1,11 +1,51 @@
 import styled from 'styled-components';
 import { flex } from '../styled-utils/mixins';
+import { ReactComponent as ClapperboardIconSVG } from '../../assets/clapperboard.svg';
 
 export const Container = styled.section`
+  margin-top: 1rem;
   height: 10rem;
   width: 100%;
 
-  background-color: white;
+  padding: 2rem;
 
   ${flex}
+`;
+
+export const Hint = styled.h2`
+  font-size: 2.2rem;
+  text-decoration: underline;
+
+  cursor: pointer;
+`;
+
+export const Data = styled.div`
+  width: 100%;
+  padding: 0.8rem;
+  border: solid 0.2rem var(--white-theme-color);
+  border-radius: 0.4rem;
+
+  ${flex}
+
+  & span {
+    font-size: 1.2rem;
+    align-self: flex-start;
+  }
+`;
+
+export const Score = styled.div`
+  position: relative;
+  margin: -1rem;
+
+  & span {
+    position: absolute;
+    left: 45%;
+    top: 48%;
+
+    font-size: 2rem;
+  }
+`;
+
+export const ClapperboardIcon = styled(ClapperboardIconSVG)`
+  height: 8.5rem;
 `;
