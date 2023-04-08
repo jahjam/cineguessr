52307987fe44e97e1c0f17a79a4e4d11a48d4805
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-04-08 01:15:32 +0100

    add card comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from './Components/Header/Header';
 import Details from './Components/Details/Details';
+import Cards from './Components/Cards/Cards';
 
 const App = () => {
   return (
     <>
       <Header />
       <Details />
+      <Cards />
     </>
   );
 };
