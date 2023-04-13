c655ed42d43c171fd44c7f9f80a93a4318adabc8
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-04-14 00:21:18 +0100

    add input feature

---
+++ src/Feature/Input/Input.tsx
@@ -0,0 +1,17 @@
+import * as Styled from './styles';
+
+import LetterInput from '../../Components/Letter-Input/Letter-Input';
+
+type InputProps = {
+  input: string[];
+};
+
+const Input = ({ input }: InputProps) => {
+  return (
+    <Styled.Form gap={1} justify="flex-start">
+      <LetterInput input={input} />
+    </Styled.Form>
+  );
+};
+
+export default Input;
