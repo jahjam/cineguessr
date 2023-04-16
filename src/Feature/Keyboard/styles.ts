ef0577624f6ffb2546b838023c901941ee5cafce
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-04-16 23:04:32 +0100

    add flash

--- src/Feature/Keyboard/styles.ts
+++ src/Feature/Keyboard/styles.ts
@@ -2,7 +2,11 @@ import styled, { css } from 'styled-components';
 
 import { ReactComponent as ReturnIconSVG } from '../../assets/keys/return.svg';
 import { ReactComponent as BackspaceIconSVG } from '../../assets/keys/Backspace.svg';
-import { flex } from '../../styled-utils/mixins';
+import { flex, flashKF, flashSpacebarKF } from '../../styled-utils/mixins';
+
+interface Props {
+  readonly flash: boolean;
+}
 
 const iconStyles = css`
   height: 1.4rem;
@@ -10,7 +14,7 @@ const iconStyles = css`
   color: var(--white-theme-color);
 `;
 
-export const IconContainer = styled.div`
+export const IconContainer = styled.div<Props>`
   height: 3rem;
   width: 3rem;
 
@@ -30,6 +34,16 @@ export const IconContainer = styled.div`
   &:nth-child(9) {
     background-color: var(--tertiary-theme-color);
   }
+
+  ${({ flash }) =>
+    flash
+      ? css`
+          animation: ${flashKF} 0.4s alternate;
+          animation-iteration-count: 2;
+        `
+      : css`
+          animation: none;
+        `};
 
   &:hover {
     transform: scale(110%);
@@ -54,7 +68,7 @@ export const BackspaceIcon = styled(BackspaceIconSVG)`
   ${iconStyles}
 `;
 
-export const Spacebar = styled.div`
+export const Spacebar = styled.div<Props>`
   height: 2.8rem;
   width: 22rem;
 
@@ -64,6 +78,16 @@ export const Spacebar = styled.div`
   border-radius: var(--primary-theme-border-radius);
 
   cursor: pointer;
+
+  ${({ flash }) =>
+    flash
+      ? css`
+          animation: ${flashSpacebarKF} 0.4s alternate;
+          animation-iteration-count: 2;
+        `
+      : css`
+          animation: none;
+        `};
 
   &:hover {
     background-color: var(--secondary-theme-color);
