360837984837e4ee56f70147d7c7381e3ced4f19
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-04-16 01:05:39 +0100

    add submit state

--- src/store/input-context.tsx
+++ src/store/input-context.tsx
@@ -1,15 +1,19 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export type ContextDefaults = {
   keyDown: string;
   input: string;
   handleSetInput: Function;
+  setSubmit: Function;
+  submit: boolean;
 };
 
 const contextDefaults = {
   keyDown: '',
   input: '',
   handleSetInput: (key: string) => {},
+  setSubmit: () => {},
+  submit: false,
 };
 
 const InputContext = React.createContext<ContextDefaults>(contextDefaults);
@@ -21,6 +25,7 @@ type Props = {
 export const InputContextProvider = ({ children }: Props) => {
   const [keyDown, setKeyDown] = useState<string>('');
   const [input, setInput] = useState<string>('');
+  const [submit, setSubmit] = useState<boolean>(false);
 
   const handleSetInput = (key: string) => {
     if (key === 'Backspace') {
@@ -31,12 +36,22 @@ export const InputContextProvider = ({ children }: Props) => {
       return setInput(prevState => (prevState += ' '));
     }
 
+    if (key === 'Enter') {
+      setInput('');
+      return setSubmit(true);
+    }
+
     setInput(prevState => (prevState += key));
   };
 
   const handleKeyDown = (e: KeyboardEvent) => {
     if (e.key === 'Backspace') {
       return setInput(prevState => prevState.slice(0, -1));
+    }
+
+    if (e.key === 'Enter') {
+      setInput('');
+      return setSubmit(true);
     }
 
     if (e.key.length !== 1) return;
@@ -49,10 +64,14 @@ export const InputContextProvider = ({ children }: Props) => {
     document.addEventListener('keydown', handleKeyDown);
   }, []);
 
+  console.log(input);
+
   const contextValue = {
     keyDown,
     input,
     handleSetInput,
+    setSubmit,
+    submit,
   };
 
   return (
