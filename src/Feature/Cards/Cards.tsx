cb95749a453514f98b8e47bffea525d88abaac0e
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-04-17 23:13:25 +0100

    add gameContext functionality for rendering cards

--- src/Feature/Cards/Cards.tsx
+++ src/Feature/Cards/Cards.tsx
@@ -1,14 +1,19 @@
 import * as Styled from './styles';
 
 import ArtCard from '../../Components/Art-Cart/Art-Cart';
+import { useContext } from 'react';
+import GameContext, { Film } from '../../store/game-context';
 
 const Cards = () => {
+  const gameContext = useContext(GameContext);
+
+  const { film } = gameContext;
+
   return (
     <Styled.Cards justify="flex-start" gap={2}>
-      <ArtCard />
-      <ArtCard />
-      <ArtCard />
-      <ArtCard />
+      {film.cards.map((card, i) => (
+        <ArtCard key={i} svgString={card.svg} />
+      ))}
     </Styled.Cards>
   );
 };
