d7e9576eb3a422a86333518d79aa28c27973138b
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-05-17 21:35:46 +0100

    add animations

--- src/Components/Letter-Card/Letter-Card.tsx
+++ src/Components/Letter-Card/Letter-Card.tsx
@@ -6,7 +6,11 @@ type LetterCardProps = {
 
 const LetterCard = ({ letter }: LetterCardProps) => {
   return (
-    <Styled.Container>
+    <Styled.Container
+      transition={{ duration: 0.5 }}
+      initial={{ translateX: 1000, opacity: 0 }}
+      animate={{ translateX: 0, opacity: 1 }}
+    >
       <span>{letter}</span>
     </Styled.Container>
   );
