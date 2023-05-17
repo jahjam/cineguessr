c33f5b39ac8bb9845702e1359220f0199663d0a7
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-05-17 17:33:54 +0100

    add alert context

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,18 @@ import './index.css';
 import { InputContextProvider } from './store/input-context';
 import { GameContextProvider } from './store/game-context';
 import { KeyDownContextProvider } from './store/key-down-context';
+import { AlertContextProvider } from './store/alert-context';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <GameContextProvider>
-      <InputContextProvider>
-        <KeyDownContextProvider>
-          <App />
-        </KeyDownContextProvider>
-      </InputContextProvider>
-    </GameContextProvider>
+    <AlertContextProvider>
+      <GameContextProvider>
+        <InputContextProvider>
+          <KeyDownContextProvider>
+            <App />
+          </KeyDownContextProvider>
+        </InputContextProvider>
+      </GameContextProvider>
+    </AlertContextProvider>
   </React.StrictMode>
 );
