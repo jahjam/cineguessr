26e1acdc6b1eb8eafa416c550476a6ddcc295c94
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-05-17 23:59:42 +0100

    add styles

--- src/Feature/Details/styles.ts
+++ src/Feature/Details/styles.ts
@@ -1,6 +1,5 @@
-import styled from 'styled-components';
-import { flex } from '../../styled-utils/mixins';
-import { ReactComponent as ClapperboardIconSVG } from '../../assets/clapperboard.svg';
+import styled, { css } from 'styled-components';
+import { flex, openKF } from '../../styled-utils/mixins';
 
 export const Container = styled.section`
   height: 10rem;
@@ -45,12 +44,79 @@ export const Score = styled.div`
   & span {
     position: absolute;
     left: 45%;
-    top: 48%;
+    top: 50%;
 
-    font-size: 2rem;
+    font-size: 2.2rem;
   }
 `;
 
-export const ClapperboardIcon = styled(ClapperboardIconSVG)`
-  height: 8.5rem;
+interface Props {
+  readonly open: boolean;
+}
+
+export const ClapperboardIcon = styled.div<Props>`
+  margin: 1rem;
+  margin-top: 2.5rem;
+  height: 4.8rem;
+  width: 8rem;
+  position: relative;
+
+  border: var(--primary-theme-border);
+  border-radius: var(--primary-theme-border-radius);
+
+  & > div:nth-child(2) {
+    height: 1.4rem;
+    width: 1.4rem;
+    border: var(--primary-theme-border);
+    border-radius: 50%;
+    position: absolute;
+    left: -0.2rem;
+    top: -0.7rem;
+    background: none;
+    background-color: #111;
+
+    z-index: 1000;
+  }
+
+  & > div {
+    background: repeating-linear-gradient(
+      135deg,
+      #111 0%,
+      #111 5%,
+      #fff 5%,
+      #fff 10%
+    );
+    height: 1rem;
+    width: 8rem;
+    position: absolute;
+    left: -0.2rem;
+    top: -0.2rem;
+    border: var(--primary-theme-border);
+    border-radius: var(--primary-theme-border-radius);
+  }
+
+  & > div:nth-of-type(1) {
+    background: repeating-linear-gradient(
+      -135deg,
+      #fff 0%,
+      #fff 5%,
+      #111 5%,
+      #111 10%
+    );
+    top: -1rem;
+
+    ${({ open }) =>
+      open
+        ? css`
+            animation-name: ${openKF};
+            transform-origin: left;
+            animation-duration: 0.25s;
+            animation-timing-function: ease-out;
+            animation-iteration-count: 2;
+            animation-direction: alternate;
+          `
+        : css`
+            animation: none;
+          `};
+  }
 `;
