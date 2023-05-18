6664fea7e2e6152b0ffbcaed401443709bac44f0
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-05-18 15:40:01 +0100

    add hint functionality

--- src/Feature/Details/Details.tsx
+++ src/Feature/Details/Details.tsx
@@ -4,9 +4,12 @@ import * as Styled from './styles';
 
 const Details = () => {
   const [openClapper, setOpenClapper] = useState(false);
+  const [toggleHint, setToggleHint] = useState(false);
+  const [hasUsedHint, setHasUsedHint] = useState(false);
+
   const gameContext = useContext(GameContext);
 
-  const { lives } = gameContext;
+  const { lives, film, handleDecrementLives } = gameContext;
 
   useEffect(() => {
     if (lives < 5) {
@@ -14,14 +17,42 @@ const Details = () => {
     }
   }, [lives]);
 
+  const handleToggleHint = () => {
+    if (!hasUsedHint) {
+      handleDecrementLives();
+      setHasUsedHint(true);
+    }
+
+    setToggleHint(!toggleHint);
+  };
+
   return (
     <Styled.Container justify="space-between">
-      <Styled.Hint>HINT</Styled.Hint>
-      <Styled.Data direction="column" gap={0.1}>
-        <span>Winning Streak: 2</span>
-        <span>Average Tries: 3.4</span>
-        <span>Least Tries: 3</span>
-        <span>Average Guess Time: 2m 53s</span>
+      <Styled.Hint onClick={handleToggleHint}>HINT</Styled.Hint>
+      <Styled.Data>
+        <Styled.DataFront
+          initial={{ rotateX: 0 }}
+          animate={{
+            rotateX: !toggleHint ? 0 : 180,
+            transition: { duration: 1 },
+          }}
+          direction="column"
+          gap={0.1}
+        >
+          <span>Winning Streak: 2</span>
+          <span>Average Tries: 3.4</span>
+          <span>Least Tries: 3</span>
+          <span>Average Guess Time: 2m 53s</span>
+        </Styled.DataFront>
+        <Styled.DataBack
+          initial={{ rotateX: -180 }}
+          animate={{
+            rotateX: toggleHint ? 0 : -180,
+            transition: { duration: 1 },
+          }}
+        >
+          <span>{film.hint}</span>
+        </Styled.DataBack>
       </Styled.Data>
       <Styled.Score>
         <Styled.ClapperboardIcon
