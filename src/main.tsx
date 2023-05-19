822b97b7c551196c537068b0ba839317a4e1c91b
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-05-19 16:40:37 +0100

    add auth context

--- src/main.tsx
+++ src/main.tsx
@@ -6,17 +6,20 @@ import { InputContextProvider } from './store/input-context';
 import { GameContextProvider } from './store/game-context';
 import { KeyDownContextProvider } from './store/key-down-context';
 import { AlertContextProvider } from './store/alert-context';
+import { AuthContextProvider } from './store/auth-context';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <AlertContextProvider>
-      <GameContextProvider>
-        <InputContextProvider>
-          <KeyDownContextProvider>
-            <App />
-          </KeyDownContextProvider>
-        </InputContextProvider>
-      </GameContextProvider>
-    </AlertContextProvider>
+    <AuthContextProvider>
+      <AlertContextProvider>
+        <GameContextProvider>
+          <InputContextProvider>
+            <KeyDownContextProvider>
+              <App />
+            </KeyDownContextProvider>
+          </InputContextProvider>
+        </GameContextProvider>
+      </AlertContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
