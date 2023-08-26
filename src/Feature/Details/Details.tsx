33e919b2c065394eac176a32767b4d3b4218aa2c
“jahjam” <elijahjamesuk@hotmail.com>
2023-08-26 15:35:12 +0100

    began to integrate user stats

--- src/Feature/Details/Details.tsx
+++ src/Feature/Details/Details.tsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useState } from 'react';
 import GameContext from '../../store/game-context';
+import UserContext from '../../store/user-context';
 import * as Styled from './styles';
 
 const Details = () => {
@@ -8,8 +9,10 @@ const Details = () => {
   const [hasUsedHint, setHasUsedHint] = useState(false);
 
   const gameContext = useContext(GameContext);
+  const userContext = useContext(UserContext);
 
   const { lives, film, handleDecrementLives } = gameContext;
+  const { user } = userContext;
 
   useEffect(() => {
     if (lives < 5) {
@@ -27,28 +30,28 @@ const Details = () => {
   };
 
   return (
-    <Styled.Container justify="space-between">
+    <Styled.Container justify='space-between'>
       <Styled.Hint onClick={handleToggleHint}>HINT</Styled.Hint>
       <Styled.Data>
         <Styled.DataFront
           initial={{ rotateX: 0 }}
           animate={{
             rotateX: !toggleHint ? 0 : 180,
-            transition: { duration: 1 },
+            transition: { duration: 1 }
           }}
-          direction="column"
+          direction='column'
           gap={0.1}
         >
-          <span>Winning Streak: 2</span>
+          <span>Winning Streak: {user?.streak}</span>
           <span>Average Tries: 3.4</span>
-          <span>Least Tries: 3</span>
+          <span>Least Takes: {user?.leastTakes}</span>
           <span>Average Guess Time: 2m 53s</span>
         </Styled.DataFront>
         <Styled.DataBack
           initial={{ rotateX: -180 }}
           animate={{
             rotateX: toggleHint ? 0 : -180,
-            transition: { duration: 1 },
+            transition: { duration: 1 }
           }}
         >
           <span>{film.hint}</span>
